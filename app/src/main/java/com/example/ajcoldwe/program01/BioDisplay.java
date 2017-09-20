ef421a675d4026b50845711eafe741ae1f92d6f5
Alex J. Coldwell <ajcoldwe@svsu.edu>
2017-09-20 15:51:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ajcoldwe/program01/BioDisplay.java
@@ -0,0 +1,13 @@
+package com.example.ajcoldwe.program01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BioDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bio_display);
+    }
+}
