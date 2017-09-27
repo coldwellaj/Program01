38680bb9eb819d0ec04a3d0163f686726d49296c
coldwellaj <coldwellaj@gmail.com>
2017-09-27 13:13:01 -0400

    Formated output

--- app/src/main/java/com/example/ajcoldwe/program01/BioDisplay.java
+++ app/src/main/java/com/example/ajcoldwe/program01/BioDisplay.java
@@ -1,7 +1,10 @@
 package com.example.ajcoldwe.program01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BioDisplay extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class BioDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bio_display);
+
+        Intent callingIntent = getIntent();
+        String message = callingIntent.getStringExtra("Message");
+
+        TextView editText = (TextView) findViewById(R.id.editText);
+        editText.setText(message);
     }
 }
--- app/src/main/java/com/example/ajcoldwe/program01/MainActivity.java
+++ app/src/main/java/com/example/ajcoldwe/program01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ajcoldwe.program01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendReport(View view) {
+        EditText firstName = (EditText)findViewById(R.id.firstName);
+        EditText lastName = (EditText)findViewById(R.id.lastName);
+        EditText school = (EditText)findViewById(R.id.school);
+        EditText gradYear = (EditText)findViewById(R.id.yearOfGraduate);
+        EditText degree = (EditText)findViewById(R.id.degree);
+        EditText major = (EditText)findViewById(R.id.major);
+        EditText activities = (EditText)findViewById(R.id.activities);
+
+        StringBuilder message = new StringBuilder();
+
+        message.append(firstName.getText() + " ");
+        message.append(lastName.getText() + " ");
+        message.append("holds a ");
+        message.append(degree.getText());
+        message.append(" in ");
+        message.append(major.getText() + " from ");
+        message.append(school.getText() + " in ");
+        message.append(gradYear.getText() + ". ");
+        message.append("Their interests include ");
+        message.append(activities.getText());
+
+        Intent intent = new Intent(this, BioDisplay.class);
+        intent.putExtra("Message", message.toString());
+        startActivity(intent);
     }
 }
